88eed05268f15ed93284f9bcd6292d09d53fe5e9
jagadeesh-tolnut <jagadeeshmouthouvel@gmail.com>
2021-07-02 09:04:51 +0530

    Linked list 3rd update

--- Linked_list/Linked_list.py
+++ Linked_list/Linked_list.py
@@ -1,11 +1,12 @@
 class Linkedlist:
     class Node:
-        __slots__ = 'element','next_node'
-        def __init__(self,element,next_node):
+        __slots__ = 'element', 'next_node'
+
+        def __init__(self, element, next_node):
             self.element = element
             self.next_node = next_node
 
-    def __int__(self):
+    def __init__(self):
         self.head = None
         self.tail = None
         self.size = 0
@@ -16,18 +17,18 @@ class Linkedlist:
     def is_empty(self):
         return self.size == 0
 
-    def add_first(self,element):
-        new_node = self.Node(element,None)
+    def add_first(self, element):
+        new_node = self.Node(element, None)
         if self.is_empty():
             self.head = new_node
             self.tail = new_node
         else:
             new_node.next_node = self.head
         self.head = new_node
-        self.size+=1
+        self.size += 1
 
-    def add_last(self,element):
-        new_node = self.Node(element,None)
+    def add_last(self, element):
+        new_node = self.Node(element, None)
         if self.is_empty():
             self.head = new_node
             self.tail = new_node
@@ -36,8 +37,8 @@ class Linkedlist:
         self.tail = new_node
         self.size += 1
 
-    def add_element(self,element,position):
-        new_node = self.Node(element,None)
+    def add_element(self, element, position):
+        new_node = self.Node(element, None)
         tpointer = self.head
         i = 1
         while i < position:
@@ -52,8 +53,8 @@ class Linkedlist:
             pass
         else:
             first = self.head.element
-            self.head = self.head.next
-            self.size -=1
+            self.head = self.head.next_node
+            self.size -= 1
             if self.is_empty():
                 self.tail = None
             return first
@@ -64,26 +65,26 @@ class Linkedlist:
         else:
             tpointer = self.head
             i = 0
-            while i<len(self)-2:
+            while i < len(self) - 2:
                 tpointer = tpointer.next_node
                 i += 1
             self.tail = tpointer
-            tpointer = tpointer.next
+            tpointer = tpointer.next_node
             last = tpointer.element
             self.tail.next_node = None
             self.size -= 1
             return last
 
-    def remove_element(self,position):
+    def remove_element(self, position):
         if self.is_empty():
             pass
         tpointer = self.head
         i = 1
-        while (i<position-1):
-            tpointer = tpointer.next
-            i+=1
-        element = tpointer.next.element
-        tpointer.next = tpointer.next.next
+        while (i < position - 1):
+            tpointer = tpointer.next_node
+            i += 1
+        element = tpointer.next_node.element
+        tpointer.next_node = tpointer.next_node.next_node
         self.size -= 1
         return element
 
@@ -91,14 +92,19 @@ class Linkedlist:
         tpointer = self.head
         while tpointer:
             print(tpointer.element, end="-->")
-            tpointer = tpointer.next
+            tpointer = tpointer.next_node
         print()
 
+
 ll = Linkedlist()
-print(ll.head)
-# ll.add_first(10)
-# ll.add_first(20)
-# ll.add_first(30)
-# ll.add_first(40)
-# ll.add_first(50)
-# ll.display()
+
+ll.add_first(10)
+ll.add_first(20)
+ll.add_first(30)
+ll.add_first(40)
+ll.add_first(50)
+ll.display()
+ll.add_last(100)
+ll.add_last(200)
+ll.add_last(300)
+ll.display()
